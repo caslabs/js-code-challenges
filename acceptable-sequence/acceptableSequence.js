453d8dcdef28226be2bf3d01ee8e94cadf0d8aba
c0p <jeraldydcascayan@gmail.com>
2018-08-26 20:32:43 -1000

    somewhat working

--- acceptable-sequence/acceptableSequence.js
+++ acceptable-sequence/acceptableSequence.js
@@ -1,3 +1,17 @@
 module.exports = function acceptableSequence(str) {
+    //check to see if string has any alphabetic
+    var theGodEquation = /[\+][a-zA-Z][\+]/ //has '+[letter]+' pattern
+    var brokenLeft = /[^+][a-z][+]/ //has + on left but not right
+    var brokenRight = /[+][a-z][^+]/ //has + on right but not left
+    var newstr = '<' + str + '>' //incase letters is in the start or end. if has, then return false
 
-}
+    if (theGodEquation.test(newstr) && !brokenLeft.test(newstr) && !brokenRight.test(newstr)) {
+        /*
+        if string has god equation and 
+        has '+' character surrounded left and right
+        return true
+        */
+            return true
+    } else 
+        return false;
+    }
