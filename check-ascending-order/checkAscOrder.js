4c49cb77d9519df3d651023bfd729168a19a36a7
c0p <jeraldydcascayan@gmail.com>
2018-08-26 20:47:21 -1000

    testing

--- check-ascending-order/checkAscOrder.js
+++ check-ascending-order/checkAscOrder.js
@@ -1,3 +1,13 @@
 module.exports = function checkAscOrder(numArray) {
   // write your code in here
+  var i=0;
+  while (i<numArray.length) {
+    var a = i;
+    var j = i+1;
+    if (a < j) {
+      i++
+    } else {
+      return false;
+    }
+  }
 }
