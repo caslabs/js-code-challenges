bbe5f1a0b996bb929b6189816feb4886bb2bda11
c0p <jeraldydcascayan@gmail.com>
2018-09-26 13:26:25 -1000

    more updated stuff

--- cookie-problem/cookieProblem.js
+++ cookie-problem/cookieProblem.js
@@ -1,3 +1,11 @@
 module.exports = function cookieProblem(array) {
   // write your code in here
+  var maxCookie = Math.max(...array);
+  var cookieSUM = 0;
+
+  array.forEach(element => {
+    cookieSUM += maxCookie - element;
+  });
+
+  return cookieSUM;
 }
--- count-by-multiples/countByMultiples.js
+++ count-by-multiples/countByMultiples.js
@@ -1,3 +1,15 @@
 module.exports = function countByMultiples(length, x) {
   // write your code in here
+  var MultipleArray = [ ];
+  var numSeq = x;
+  console.log(10%2);
+
+  while (MultipleArray.length < length) {
+      if (numSeq % x == 0) {
+        MultipleArray.push(numSeq)
+      }
+      numSeq++;
+  }
+
+  return MultipleArray;
 }
